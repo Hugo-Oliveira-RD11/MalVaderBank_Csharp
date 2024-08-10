56fa60a7463818db718f1d5c7c3672f62abb0ffe
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2024-08-10 15:01:44 -0300

    separete a Funcionario

---
+++ Models/Funcionario.cs
@@ -0,0 +1,11 @@
+using Models;
+
+public class Funcionario:Pessoa{
+    public enum CargoFuncionario{gerente=1, subgerente=0}
+    protected uint codigo {get;set;}
+    public CargoFuncionario cargo;
+    Funcionario()
+    {
+        cargo = CargoFuncionario.subgerente;
+    }
+}
